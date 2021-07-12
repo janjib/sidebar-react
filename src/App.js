b3720e34d5cfaf2fc88d5a41b9a7795620033cbe
janjib <janjib04@gmail.com>
2021-07-12 23:49:43 +0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Components/Sidebar';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import Overview from './pages/Overview';
+import Reports from './pages/Reports';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Sidebar/>
+      <Switch>
+        <Route path='/sidebar-react' exact component={Overview} />
+        <Route path='/sidebar-react/reports1' exact component={Reports} />
+      </Switch>
+      </Router>
+    </>
   );
 }
 
